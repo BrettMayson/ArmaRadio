ddf42a32e034ded217a257373c5ada37b82a1972
Brett <brett@mayson.io>
2022-03-24 20:40:14 -0600

    remove broken stations

--- addons/interface/CfgRadioStations.hpp
+++ addons/interface/CfgRadioStations.hpp
@@ -11,17 +11,9 @@ class CfgRadioStations {
 		name = "PulseEDM Dance Music";
 		url = "http://pulseedm.cdnstream1.com:8124/1373_128";
 	};
-	class LifeChillMusic {
-		name = "Life Chill Music";
-		url = "http://aska.ru-hoster.com:8053/autodj";
-	};
 	class LifeIreland {
 		name = "Live Ireland";
 		url = "http://192.111.140.11:8058/stream?type=http&nocache=325927";
-	};
-	class PuroConjunto {
-		name = "Puro Conjunto";
-		url = "http://167.114.64.181:9327/stream?type=http&nocache=32231";
 	};
 	class VeniceClassic {
 		name = "Venice Classic Radio Italia";
